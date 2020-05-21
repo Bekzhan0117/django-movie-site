5ccf5f3ca91cb45ced2d98c12ef585f7a728644d
Bekzhan0117 <61686269+Bekzhan0117@users.noreply.github.com>
2020-05-22 00:43:47 +0600

    Оптимизация, удалил movie_list.html, переименовал movies.html в movie_list.html, изменил структуру классаов views.py убрал функций.git add .

--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from .models import Movie
 
-class MoviesView(View):
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movies.html", {"movie_list": movies})
+class MoviesView(ListView):
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    #template_name = "movies/movie_list.html"
 
-class MovieDetailView(View):
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, "movies/movie_detail.html", {"movie": movie})
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = "url"
+
